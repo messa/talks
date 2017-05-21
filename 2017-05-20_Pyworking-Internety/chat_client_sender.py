034ba71822de8506e2af960a4ec54a5cde022a5f
Petr Messner <petr.messner@gmail.com>
2017-05-22 00:57:01 +0200

    chat sender: sendall

--- 2017-05-20_Pyworking-Internety/chat_client_sender.py
+++ 2017-05-20_Pyworking-Internety/chat_client_sender.py
@@ -11,17 +11,12 @@ def main():
     args = p.parse_args()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((args.host, int(args.port)))
-    buf = b''
     while True:
         msg = input('zadej zpravu: ')
         if not msg:
             continue
         to_send = msg.encode() + b'\n'
-        while to_send:
-            n = s.send(to_send)
-            if n == 0:
-                raise Exception('Send failed')
-            to_send = to_send[n:]
+        s.sendall(to_send)
 
 
 if __name__ == '__main__':
